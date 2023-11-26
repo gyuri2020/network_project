76b686ba7045bb4216430f9b5af7f0741160e730
LeeGyuRi <lj01081512@gmail.com>
2023-11-26 16:15:01 +0900

    feat: tcp

---
+++ client_tcp.py
@@ -0,0 +1,20 @@
+from socket import *
+import sys
+
+
+
+def tcp_client(serverIP, serverPort):
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverIP, serverPort)) #only tcp, not udp
+
+    while True:
+        message = "hello"
+        clientSocket.send(message.encode())
+        modifiedMessage = clientSocket.recv(2048).decode()
+        print(modifiedMessage)
+        if modifiedMessage == "EXIT":
+            break
+
+    clientSocket.close()
+
+tcp_client(sys.argv[1], int(sys.argv[2]))
---
+++ server_tcp.py
@@ -0,0 +1,37 @@
+from socket import *
+import threading
+import sys
+
+
+def handle_client(connectionSocket, addr):
+    cnt = 0
+    while True:
+
+        message = connectionSocket.recv(2048).decode() # always use thread for multiple client
+        modifiedMessage = message.upper()
+        cnt += 1
+        if cnt == 10: # for test
+            modifiedMessage = "EXIT"
+        connectionSocket.send(modifiedMessage.encode())
+        if modifiedMessage == "EXIT":
+            break
+    connectionSocket.close()
+    return 
+
+def tcp_server(serverIP, serverPort):
+    serverSocket = socket(AF_INET, SOCK_STREAM) #TCP
+    serverSocket.bind((serverIP, serverPort))
+    serverSocket.listen(1)
+    print("The server is ready to receive")
+
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        #I want to use thread here
+        thread = threading.Thread(target=handle_client, args=(connectionSocket, addr))
+        thread.start()
+        
+
+tcp_server(sys.argv[1], int(sys.argv[2]))
+
+
+    
