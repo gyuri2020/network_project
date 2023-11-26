4a85c4d4dd2fe01729a57d2911c1c8c9ec916036
LeeGyuRi <lj01081512@gmail.com>
2023-11-26 17:08:11 +0900

    dd

--- tcp_net.py
+++ tcp_net.py
@@ -18,20 +18,11 @@ class ManyTCPConnectionTopology(Topo):
         self.addLink(server, switch, cls=TCLink, bw=10000000, delay='0.1ms', loss=0.01)
 
 
-def measure_bandwidth(net, client, server_ip):
-    bw_result = net.iperf([client, net.getNodeByName('server')], seconds=5)
-    info(f"{client} - Bandwidth: {bw_result[0]}")
+def ping_to_server(client, server):
+    result = client.ping(server, timeout=10)
+    print(result)
 
-def measure_loss(net, client, server_ip):
-    loss_result = client.cmd(f"ping -c 5 {server_ip} | grep packet | awk '{{print $6}}'")
-    info(f"{client} - Loss: {loss_result}")
 
-def connect_with_server(client, server_ip, server_port):
-    start_time = time.time()
-    client.cmd(f'python3 client_tcp.py {server_ip} {server_port}')
-    end_time = time.time()
-    rtt = (end_time - start_time) * 1000
-    print(f"{client} - Round-trip time: {rtt:.2f} ms")
 
 def main():
     topo = ManyTCPConnectionTopology()
@@ -43,9 +34,9 @@ def main():
     server_port = 12000
     server_ip = "10.0.0.1"
     server.setIP(intf="server-eth0", ip="10.0.0.1/24")
-    server.cmd(f'python3 server_tcp.py {server_ip} {server_port} &')
+    #server.cmd(f'python3 server_tcp.py {server_ip} {server_port} &')
 
-    time.sleep(3)
+    #time.sleep(3)
 
     clients = []
     threads = []
@@ -56,19 +47,10 @@ def main():
         clients.append(h)    
 
     for client in clients:
-        thread = threading.Thread(target=connect_with_server, args=(client, server_ip, server_port))
+        thread = threading.Thread(target=ping_to_server, args=(client, server))
         threads.append(thread)
         thread.start()
 
-    for client in clients:
-        thread = threading.Thread(target=measure_bandwidth, args=(net, client, server_ip))
-        threads.append(thread)
-        thread.start()
-
-    for client in clients:
-        thread = threading.Thread(target=measure_loss, args=(net, client, server_ip))
-        threads.append(thread)
-        thread.start()
 
     for thread in threads:
         thread.join()
