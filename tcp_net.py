e08290ad614345d065ec9ce7c9bdae8a53e7756f
LeeGyuRi <lj01081512@gmail.com>
2023-11-26 18:01:56 +0900

    dd

--- tcp_net.py
+++ tcp_net.py
@@ -44,7 +44,7 @@ def main():
         h = net.addHost(f'h{i+1}', cls=Host, defaultRoute=None)
         link = net.addLink(h, net.get('s1'), cls=TCLink, bw=100, delay='0.1ms', loss=0.01)
         h.setIP(intf=f'h{i+1}-eth0', ip=f"10.0.0.{i+2}/24")
-        client.connect(server)
+        h.connectionsTo(server)
         clients.append(h)    
 
     for client in clients:
