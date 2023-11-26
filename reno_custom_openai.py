c3fe56aa4f96bf79701996198c9f59cfc2e2c807
LeeGyuRi <lj01081512@gmail.com>
2023-11-26 21:43:24 +0900

    dd

--- reno_custom_openai.py
+++ reno_custom_openai.py
@@ -4,47 +4,62 @@
 
 void tcp_reno_init(struct sock *sk)
 {
-    /* 혼잡 제어와 관련된 변수 초기화 */
-    tcp_sk(sk)->snd_ssthresh = 65535; // 보통 높은 값을 가짐
-    tcp_sk(sk)->snd_cwnd = 10; // 초반 혼잡 윈도우를 10으로 설정
+    /* Initialize congestion control specific variables here */
+    tcp_sk(sk)->snd_ssthresh = TCP_INFINITE_SSTHRESH; // Typically, this is a high value
+    tcp_sk(sk)->snd_cwnd = 3; // Start with a congestion window of 3
 }
 
 u32 tcp_reno_ssthresh(struct sock *sk)
 {
-    /* 혼잡 윈도우 절반으로 줄임 */
+    /* Halve the congestion window, min 2 */
     const struct tcp_sock *tp = tcp_sk(sk);
     return max(tp->snd_cwnd >> 1U, 2U);
 }
 
+u32 tcp_custom_slow_start(struct tcp_sock *tp, u32 acked) {
+
+    u32 diff = tp->snd_ssthresh - tp->snd_cwnd;
+    u32 increase_factor = 1 + (diff / 10); 
+    u32 cwnd = min(tp->snd_cwnd + (acked * increase_factor), tp->snd_ssthresh);
+
+    acked = max(acked - (cwnd - tp->snd_cwnd), 0 );
+    tp->snd_cwnd = min(cwnd, tp->snd_cwnd_clamp);
+
+    return acked;
+}
+
+
 void tcp_reno_cong_avoid(struct sock *sk, u32 ack, u32 acked)
 {
     struct tcp_sock *tp = tcp_sk(sk);
+    printk(KERN_INFO "tp->snd_cwnd is %d\n", tp->snd_cwnd);
 
     if (!tcp_is_cwnd_limited(sk))
         return;
 
     if (tp->snd_cwnd <= tp->snd_ssthresh) {
-        /* "느린 시작" 상태에서 ACK된 패킷 수만큼 혼잡 윈도우 크기 증가 */
-        acked = tcp_slow_start(tp, acked);
+        /* In "slow start", cwnd is increased by the number of ACKed packets */
+        acked = tcp_custom_slow_start(tp, acked);
         if (!acked)
             return;
     } else {
-        /* "혼잡 회피" 상태에서 혼잡 윈도우 크기 증가
-         * RTT(왕복 시간)를 대략적으로 나눈 것으로 패킷 크기를 증가시킴 */
+        /* In "congestion avoidance", cwnd is increased by 1 full packet
+         * per round-trip time (RTT), which is approximated here by the number of
+         * ACKed packets divided by the current congestion window. */
         tcp_cong_avoid_ai(tp, tp->snd_cwnd, acked);
     }
 
-    /* 혼잡 윈도우 크기가 최대 허용 값보다 크지 않도록 조정 */
+    /* Ensure that cwnd does not exceed the maximum allowed value */
     tp->snd_cwnd = min(tp->snd_cwnd, tp->snd_cwnd_clamp);
 }
 
 u32 tcp_reno_undo_cwnd(struct sock *sk)
 {
-    /* 혼잡 회피 중 변경된 혼잡 윈도우 크기를 되돌림 */
+    /* Undo the cwnd changes during congestion avoidance if needed */
     return tcp_sk(sk)->snd_cwnd;
 }
 
-/* 혼잡 제어 알고리즘을 위한 훅을 포함하는 구조체 */
+/* This structure contains the hooks to our congestion control algorithm */
 static struct tcp_congestion_ops tcp_reno_custom = {
     .init           = tcp_reno_init,
     .ssthresh       = tcp_reno_ssthresh,
@@ -55,20 +70,20 @@ static struct tcp_congestion_ops tcp_reno_custom = {
     .name           = "reno_custom",
 };
 
-/* 모듈 초기화 함수 */
+/* Initialization function of this module */
 static int __init tcp_reno_module_init(void)
 {
-    /* 새로운 혼잡 제어 등록 */
+    /* Register the new congestion control */
     BUILD_BUG_ON(sizeof(struct tcp_congestion_ops) != sizeof(struct tcp_congestion_ops));
     if (tcp_register_congestion_control(&tcp_reno_custom))
         return -ENOBUFS;
     return 0;
 }
 
-/* 모듈 정리 함수 */
+/* Cleanup function of this module */
 static void __exit tcp_reno_module_exit(void)
 {
-    /* 혼잡 제어 등록 해제 */
+    /* Unregister the congestion control */
     tcp_unregister_congestion_control(&tcp_reno_custom);
 }
 
@@ -78,3 +93,4 @@ module_exit(tcp_reno_module_exit);
 MODULE_AUTHOR("nethw");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("TCP Reno Congestion Control");
+                                                  
