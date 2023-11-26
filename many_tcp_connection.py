ce1038e47f0e5d591a7a590d365e0c9fece00e1e
LeeGyuRi <lj01081512@gmail.com>
2023-11-26 14:50:56 +0900

    modify

--- many_tcp_connection.py
+++ many_tcp_connection.py
@@ -14,8 +14,21 @@ class ManyTCPConnectionTopology(Topo):
 
         self.addLink(server, switch, cls=TCLink, bw=10000000, delay='0.1ms', loss=0.01)
 
-def iperf_client(host, server_ip, port):
+def iperf_client(host, server_ip, port, bandwidth):
     host.cmd(f"iperf -c {server_ip} -p {port} -t 10 &")
+
+def measure_connection(net, client, server_ip):
+    client_bw, _ = net.iperf([client, net.getNodeByName('server')], seconds=10)
+    server_bw, _ = net.iperf([net.getNodeByName('server'), client], seconds=10)
+    
+    client_loss = client.cmd(f"ping -c 10 {server_ip} | grep packet | awk '{{print $6}}'")
+    server_loss = net.getNodeByName('server').cmd(f"ping -c 10 {client.IP()} | grep packet | awk '{{print $6}}'")
+    
+    client_rtt = client.cmd(f"ping -c 10 {server_ip} | grep rtt | awk '{{print $4}}'")
+    server_rtt = net.getNodeByName('server').cmd(f"ping -c 10 {client.IP()} | grep rtt | awk '{{print $4}}'")
+
+    info(f"Client {client} to Server - Bandwidth: {client_bw}, Loss: {client_loss}, RTT: {client_rtt}")
+    info(f"Server to Client {client} - Bandwidth: {server_bw}, Loss: {server_loss}, RTT: {server_rtt}")
 
 def main():
     topo = ManyTCPConnectionTopology()
@@ -24,24 +37,28 @@ def main():
     net.start()
 
     server = net.getNodeByName('server')
-
     server.setIP(intf="server-eth0", ip="10.0.0.1/24")
 
     server_ip = "10.0.0.1"
     server_port = 5001
 
+    clients = []
     for i in range(64):
         h = net.addHost(f'h{i+1}', cls=Host, defaultRoute=None)
         link = net.addLink(h, net.get('s1'), cls=TCLink, bw=int(f"{i}0000"), delay='0.1ms', loss=0.01)
         h.setIP(intf=f'h{i+1}-eth0', ip=f"10.0.0.{i+2}/24")
-        threading.Thread(target=iperf_client, args=(h, server_ip, server_port)).start()
-        
-        # 각 링크의 속성 가져오기
+        clients.append(h)
+        threading.Thread(target=iperf_client, args=(h, server_ip, server_port, f"{i}0000")).start()
+
         bw = link.intf1.params['bw']
         delay = link.intf1.params['delay']
         loss = link.intf1.params['loss']
 
         info(f"Link {i+1} - Bandwidth: {bw}, Delay: {delay}, Loss: {loss}\n")
+
+    # 각 connection의 대역폭, 손실률, RTT 계산
+    for client in clients:
+        threading.Thread(target=measure_connection, args=(net, client, server_ip)).start()
 
     CLI(net)
     net.stop()
